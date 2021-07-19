56fd056b0122407d010acb223cd147ac415d6a6d
Fatimata-dev <dramefatim7@gmail.com>
2021-07-19 14:46:15 +0200

    exercice

---
+++ angular-exercice/src/app/core/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  titre = "Bonjour le jour";
+     message = "Interpolation";
+     imgUrl = 'https://www.atalante.fr/upload_1000/DSCF4994.jpg';
+}
